8c14ca536f3de84305185ccb4e6c61ec91132d29
Kate Sanders <SandersKM@Hendrix.edu>
2019-11-03 14:48:05 -0600

    moved some test code out of flow

--- src/flow_calculations/test.py
+++ src/flow_calculations/test.py
@@ -20,3 +20,33 @@ class MyTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+h: float = 0.1
+x0: float = 4
+alpha: float = 0.5
+sourceWeight = [1, 1]
+sourceY = [1, 5]
+sinkX: float = 4
+
+
+network = Network()
+network.addSource(Node(1,Point(0,5)))
+network.addSource(Node(1,Point(0,1)))
+
+network.addSink(Node(2, Point(4,3)))
+
+network.addBifurcation(Point(4,3))
+
+flow = Flow(h, alpha, network)
+i = 0
+minimized = minimize_scalar(flow.calculateG).x
+print(f"minimized {i}: {minimized} \t {flow.calculateG(minimized)}\n")
+while i < 1000:
+    network.popBifurcation()
+    network.addBifurcation(Point(minimized, 3))
+    flow = Flow(h, alpha,network)
+    i += 1
+    minimized = minimize_scalar(flow.calculateG).x
+    if (i % 1 == 0):
+        print(f"minimized {i}: {minimized} \t {flow.calculateG(minimized)}")
