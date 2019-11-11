c103078f143ab463871b8615bbc9307cc843c514
Kate Sanders <SandersKM@Hendrix.edu>
2019-11-10 22:30:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'sys', 'math', 'ipywidgets', 'bokeh', 'scipy', 'typing', 'IPython'
+    'ipywidgets', 'bokeh', 'scipy', 'typing', 'IPython'
 ]
 
 # What packages are optional?
