3fb1a25e8daa68b5d30633e48825cf817e8d754d
Kate Sanders <SandersKM@Hendrix.edu>
2019-11-10 22:35:00 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'bifurcation-finder'
+NAME = 'bifurcationFinder'
 DESCRIPTION = 'Senior Thesis: Programmatically finding bifurcation (branching) points for flow networks.'
 URL = 'https://github.com/SandersKM/bifurcation-finder'
 EMAIL = 'sanderkm@hendrix.edu'
