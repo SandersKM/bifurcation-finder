22f3b227c676f159df3df58896813dae9d060bc8
Kate Sanders <SandersKM@Hendrix.edu>
2019-11-12 22:54:56 -0600

    changing function for get minimum to hold more info

--- src/flow_calculations/flow_minimizer.py
+++ src/flow_calculations/flow_minimizer.py
@@ -8,47 +8,41 @@ from scipy.optimize import minimize_scalar
 
 class FlowMinimizer:
 
-    def get_minimum_flow(self, fl: Flow, max_iterations: int = 10000, difference_cuttoff: float = .0000001):
+    def __init__(self, fl: Flow, max_iterations: int = 10000, difference_cuttoff: float = .0000001):
+        self.fl: Flow = fl
+        self.max_iterations = max_iterations
+        self.difference_cuttoff = difference_cuttoff
+        self.steps = []
+        self.cost = []
+        self.theta = []
+    
+
+    def get_minimum_flow(self):
         i: int = 0
         flow_diff: float = 1
-        minimized = minimize_scalar(fl.calculateG).x
-        new_value = fl.calculateG(minimized)
-        s1 = fl.network.getSources()[0]
-        s2 = fl.network.getSources()[1]
-        start = fl.network.getSourcePoints()[0]
-        bifurcation = fl.network.getBifurcationPoints()[0]
-        s1.getThetaSelf(start, b)
-        print(f"new value = {new_value} \t minimized = {minimized} \t {s1.getThetaSelf(start, b) + s1.getThetaSelf(start, b)}")
+        minimized = minimize_scalar(self.fl.calculateG).x
+        new_value = self.fl.calculateG(minimized)
+        s1 = self.fl.network.getSources()[0]
+        s2 = self.fl.network.getSources()[1]
+        start = self.fl.network.getSourcePoints()[0]
+        bifurcation = self.fl.network.getBifurcationPoints()[0]
+        self.theta = [s1.getThetaSelf(start, bifurcation) + s1.getThetaSelf(start, bifurcation)]
+        self.steps = [self.fl.oldBifurcationPoint.getX(), minimized]
+        self.cost = [new_value]
         net: Network = fl.getNetwork()
-        while i < max_iterations and abs(flow_diff) > difference_cuttoff:
+        while i < self.max_iterations and abs(flow_diff) > self.difference_cuttoff:
             b = net.popBifurcation()
             bifurcation = Point(minimized, b.getY())
             net.addBifurcation(bifurcation)
-            fl.updateNetwork(net)
+            self.fl.updateNetwork(net)
             i += 1
             minimized = minimize_scalar(fl.calculateG).x
+            self.steps.append(minimized)
             old_value: float = new_value
-            new_value = fl.calculateG(minimized)
+            new_value = self.fl.calculateG(minimized)
             flow_diff = new_value - old_value
-            print(f"new value = {new_value} \t minimized = {minimized} \t {s1.getThetaSelf(start, b) + s1.getThetaSelf(start, b)}")
-        return fl
+            self.cost.append(new_value)
+            self.theta.append(s1.getThetaSelf(start, bifurcation) + s1.getThetaSelf(start, bifurcation))
+        return self.fl
 
-    def get_flow_steps(self, fl: Flow, max_iterations: int = 10000, difference_cuttoff: float = .0000001):
-        i: int = 0
-        flow_diff: float = 1
-        minimized = minimize_scalar(fl.calculateG).x
-        new_value = fl.calculateG(minimized)
-        flow_steps = [fl.oldBifurcationPoint.getX(), minimized]
-        net: Network = fl.getNetwork()
-        while i < max_iterations and abs(flow_diff) > difference_cuttoff:
-            net.popBifurcation()
-            net.addBifurcation(Point(minimized, 3))
-            fl.updateNetwork(net)
-            i += 1
-            minimized = minimize_scalar(fl.calculateG).x
-            flow_steps.append(minimized)
-            old_value: float = new_value
-            new_value = fl.calculateG(minimized)
-            flow_diff = new_value - old_value
-        return flow_steps
 
