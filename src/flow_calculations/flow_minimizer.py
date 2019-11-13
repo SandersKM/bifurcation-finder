910053cf8bd56adb0c33f1cf55eccdad53230f49
Kate Sanders <SandersKM@Hendrix.edu>
2019-11-12 19:19:58 -0600

    adding print statements

--- src/flow_calculations/flow_minimizer.py
+++ src/flow_calculations/flow_minimizer.py
@@ -13,16 +13,24 @@ class FlowMinimizer:
         flow_diff: float = 1
         minimized = minimize_scalar(fl.calculateG).x
         new_value = fl.calculateG(minimized)
+        s1 = fl.network.getSources()[0]
+        s2 = fl.network.getSources()[1]
+        start = fl.network.getSourcePoints()[0]
+        bifurcation = fl.network.getBifurcationPoints()[0]
+        s1.getThetaSelf(start, b)
+        print(f"new value = {new_value} \t minimized = {minimized} \t {s1.getThetaSelf(start, b) + s1.getThetaSelf(start, b)}")
         net: Network = fl.getNetwork()
         while i < max_iterations and abs(flow_diff) > difference_cuttoff:
             b = net.popBifurcation()
-            net.addBifurcation(Point(minimized, b.getY()))
+            bifurcation = Point(minimized, b.getY())
+            net.addBifurcation(bifurcation)
             fl.updateNetwork(net)
             i += 1
             minimized = minimize_scalar(fl.calculateG).x
             old_value: float = new_value
             new_value = fl.calculateG(minimized)
             flow_diff = new_value - old_value
+            print(f"new value = {new_value} \t minimized = {minimized} \t {s1.getThetaSelf(start, b) + s1.getThetaSelf(start, b)}")
         return fl
 
     def get_flow_steps(self, fl: Flow, max_iterations: int = 10000, difference_cuttoff: float = .0000001):
