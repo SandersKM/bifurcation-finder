d85f26c0b406c7cdc3afd236f0f8fdee7b93b277
Kate Sanders <SandersKM@Hendrix.edu>
2019-11-20 13:58:50 -0600

    adding some room for error in the 90 degree thing

--- src/flow_calculations/flow_minimizer.py
+++ src/flow_calculations/flow_minimizer.py
@@ -22,7 +22,7 @@ class FlowMinimizer:
             return False
         #if abs(flow_diff) < self.difference_cuttoff:
         #    return False
-        if self.theta[-1] > 90 - flow_diff:
+        if self.theta[-1] > 90 - self.difference_cuttoff:
             return False
         return True
         
