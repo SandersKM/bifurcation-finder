a0224b84d5f03c0670ebcb51879b6968cff92ff8
Kate Sanders <SandersKM@Hendrix.edu>
2020-04-07 09:53:12 -0500

    fixes

--- src/bernot_calculations/bernot_graph.py
+++ src/bernot_calculations/bernot_graph.py
@@ -64,9 +64,9 @@ class Bernot_Graph:
         right_closeness = abs(source_list[0].get_distance_to(source_list[1]))
         left_closeness = abs(source_list[-1].get_distance_to(source_list[-2]))
         across_closeness = abs(source_list[-1].get_distance_to(source_list[0]))
-        if right_closeness < left_closeness and right_closeness < across_closeness:
+        if right_closeness <= left_closeness and right_closeness <= across_closeness:
             return self.subgraph_with_sources(source_list[0], source_list[1])
-        elif left_closeness < right_closeness and left_closeness < across_closeness:
+        elif left_closeness <= right_closeness and left_closeness <= across_closeness:
             return self.subgraph_with_sources(source_list[-2], source_list[-1])
         return self.subgraph_with_sources(source_list[-1], source_list[0])
 
@@ -102,7 +102,8 @@ class Bernot_Graph:
             segments = []
         else:
             segments = self.visualization_steps[-1][1]["segments"].copy()
-        segments.append(big_line)
+        if big_line not in segments:
+            segments.append(big_line)
         self.visualization_steps.append(("find bifurcation point", {"points": points, \
             "circles": center_circle, "segments": segments}))
         segments2 = segments.copy()
